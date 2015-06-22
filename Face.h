b75957aa274abc07b67511d1d1e79c45ab304cd3
k05mang <kmango2015@gmail.com>
2015-06-22 15:08:02 -0400

    added comments to face class

--- Face.h
+++ Face.h
@@ -5,7 +5,7 @@
 class Face
 {
 private:
-	HalfEdge e1, e2, e3;
+	HalfEdge e1, e2, e3;//edges of this face
 public:
 	Face(int v1, int v2, int v3);
 	~Face();
@@ -14,7 +14,15 @@ public:
 	Face(Face&& moveTarget);
 	Face& operator= (Face&& moveTarget);
 
+	/*
+		Stores the indices of this face that represent just the triangle primitive
+	*/
 	void storePrim(std::vector<int>& storage);
+
+	/*
+		Stores the indices of this face that represent this triangle and it's adjacent faces
+		in a manner complient with OpenGL TRIANGLE_ADJACENCY
+	*/
 	void storePrimAdj(std::vector<int>& storage);
 };
 
